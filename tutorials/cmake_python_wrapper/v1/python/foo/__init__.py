364421cdfff4e0bc4fe2d495f774c7dc429e3100
Jean Jacquemier <jacquem@lapp.in2p3.fr>
2015-11-19 14:42:47 +0100

    remove unused files

--- tutorials/cmake_python_wrapper/v1/python/foo/__init__.py
+++ tutorials/cmake_python_wrapper/v1/python/foo/__init__.py
@@ -4,10 +4,7 @@ import os
 
 __all__ = ['square']
 
-_path = os.path.dirname(__file__)
-#lib = np.ctypeslib.load_library('foo')
 lib = ctypes.cdll.LoadLibrary("libfoo.so")
-
 lib.square.restype = ctypes.c_int
 lib.square.argtypes = [ctypes.c_int]
 
